043a9789653d376e6ea589f28d03dfb6d6865730
Abou djire <Abou djire@192.168.56.1>
2022-06-30 22:29:07 -0400

    Mon premier commit pour mon projet Test

---
+++ src/com/ets/test/Etudiant.java
@@ -0,0 +1,14 @@
+package com.ets.test;
+
+public class Etudiant {
+
+	public static void main(String[] args) {
+	
+		
+		String dramane = "dramane a étudiant mange";
+		
+	    System.out.println(dramane);
+	    
+	}
+
+}

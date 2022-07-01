7f4f51bed1398d212d08b6ac898ffbfe3e3ad298
Abou djire <Abou djire@192.168.56.1>
2022-06-30 23:03:06 -0400

    Mon 2e commit

--- src/com/ets/test/Etudiant.java
+++ src/com/ets/test/Etudiant.java
@@ -5,7 +5,7 @@ public class Etudiant {
 	public static void main(String[] args) {
 	
 		
-		String dramane = "dramane a étudiant mange";
+		String dramane = "dramane  dit un  étudiant mange";
 		
 	    System.out.println(dramane);
 	    
